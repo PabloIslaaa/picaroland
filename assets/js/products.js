200af4aba659c1184a7094fab0c636165c196145
PabloIslaaa <mrpablo_yt@outlook.com>
2025-11-03 19:58:49 +0100

    Add products data to products.js

---
+++ assets/js/products.js
@@ -0,0 +1,7 @@
+// assets/js/products.js
+window.PRODUCTS = [
+  { id: "cam-001", name: "Camiseta básica", price: 14.99, image: "img/products/camiseta-basica.jpg", tags: ["camisetas","básicos"] },
+  { id: "sud-002", name: "Sudadera unisex", price: 29.90, image: "img/products/sudadera.jpg", tags: ["sudaderas"] },
+  { id: "pan-003", name: "Pantalón chino", price: 39.00, image: "img/products/pantalon-chino.jpg", tags: ["pantalones"] },
+  { id: "ves-004", name: "Vestido midi", price: 49.00, image: "img/products/vestido-midi.jpg", tags: ["vestidos"] }
+];
